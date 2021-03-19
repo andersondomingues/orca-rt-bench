ecd6181301953748716e55ef5d510e9b9762d3f2
Anderson Domingues <ti.andersondomingues@gmail.com>
2021-03-19 15:47:33 -0300

    fixed compilation scripts, compilation artifacts cleanup, and windows compat

---
+++ apps/orca-rt-tdmgen/include/main.hpp
@@ -0,0 +1,15 @@
+/*
+ * main.hpp
+ *
+ *  Created on: 19 de mar. de 2021
+ *      Author: Admin
+ */
+
+#ifndef APPS_ORCA_RT_TDMGEN_INCLUDE_MAIN_HPP_
+#define APPS_ORCA_RT_TDMGEN_INCLUDE_MAIN_HPP_
+
+
+
+
+
+#endif /* APPS_ORCA_RT_TDMGEN_INCLUDE_MAIN_HPP_ */
